fb29855246d3cb62dbdf739863e876d6efd56a2f
matiaslog <flaviomatiasnf@gmail.com>
2024-10-30 23:00:40 -0300

    nami

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
-    if (window.scrollY > 400 && window.scrollY < 1100) {
+    if (window.scrollY > 400 && window.scrollY <= 1300) {
         navbar.classList.add('sobre');
     } else
     {
         navbar.classList.remove('sobre');
     }
-    if (window.scrollY > 1100) {
+    if (window.scrollY >= 1300) {
         navbar.classList.add('mapa');
     } else{
         navbar.classList.remove('mapa');
